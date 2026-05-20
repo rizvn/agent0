be7df1215a93aabf8a9f4322a03478cda706a782
Riz <Riz>
2026-05-20 10:35:18 +0100

    .

--- src/agent/config.go
+++ src/agent/config.go
@@ -15,17 +15,30 @@ type Config struct {
 }
 
 func NewConfigFromEnv(filePath string) (*Config, error) {
+
+	// Load environment variables from the specified file
+	// This will not override existing environment variables
 	err := godotenv.Load(filePath)
+
 	if err != nil {
 		return nil, util.DetailedError("Unable to load environment vars", err)
 	}
+
+	// create config struct
 	config := &Config{}
 
+	// populate config struct with environment variables
 	config.LLMApiBaseUrl = os.Getenv("LLM_BASE_URL")
 	config.LLMApiKey = os.Getenv("LLM_API_KEY")
 	config.LLMModel = os.Getenv("LLM_MODEL")
 	config.LogLevel = os.Getenv("LOG_LEVEL")
 
+	// set default log level if not provided
+	if config.LogLevel == "" {
+		config.LogLevel = "INFO"
+	}
+
+	// validate required environment variables
 	if config.LLMApiBaseUrl == "" {
 		return nil, util.DetailedError("Env variable LLM_BASE_URL not found", nil)
 	}
